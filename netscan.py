3ae32cf283590f8e64df952e229d80002e00357a
Jing Shao <jing_shao@yahoo.com>
2017-10-26 22:25:04 -0500

    Added netscan.py

---
+++ netscan.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+
+import time
+import multiprocessing
+import subprocess
+import netaddr
+import os
+import sys
+import argparse
+
+CONFIG_FILE= os.environ['HOME'] + "/.known_hosts"
+MAC_ADDR_STR="MAC Address"
+DEFAULT_NET="192.168.3.0"
+
+oui_map={}
+mac_map={}
+ip_list=[]
+known_hosts = {}
+
+def pinger( job_q, results_q ):
+    DEVNULL = open(os.devnull,'w')
+    while True:
+        ip = job_q.get()
+        if ip is None: break
+
+        try:
+            subprocess.check_call(['ping','-c5',ip],
+                                  stdout=DEVNULL)
+            results_q.put(ip)
+        except:
+            pass
+
+def init_oui_map():
+    global oui_map
+    with open("oui.map", "r") as ins:
+        for line in ins:
+	    fields = line.split(":")
+	    mac = fields[0].strip()
+            company = fields[1].strip()
+            oui_map[mac]=company
+    ins.close()
+
+def get_company(oui):
+    if oui_map.has_key(oui):
+        return oui_map[oui]
+    else:
+        return ""
+
+def get_oui(mac):
+    new_oui=[]
+    oui=mac[:8].replace(":","")
+    ouiArray =  bytearray(oui)
+    for i in range(len(ouiArray)):
+        if ((ouiArray[i] >= ord('a')) and (ouiArray[i] <= ord('f'))):
+           ouiArray[i] = ord('A') + ouiArray[i] - ord('a')
+
+    return str(ouiArray)
+
+def get_mac():
+    global mac_map
+    with open("/proc/net/arp", "r") as ins:
+        for line in ins:
+	    fields = line.split()
+ 
+            if len(fields) == 6:
+	        ip=fields[0]
+		if (fields[3] != "00:00:00:00:00:00"):
+                    mac_map[ip] = fields[3]
+    ins.close()
+
+def do_ping(subnet):
+    global ip_list
+    pool_size = 255
+
+    jobs = multiprocessing.Queue()
+    results = multiprocessing.Queue()
+
+    pool = [ multiprocessing.Process(target=pinger, args=(jobs,results))
+             for i in range(pool_size) ]
+
+    for p in pool:
+        p.start()
+
+    for i in range(1,255):
+        ip_str = subnet + "." + str(i)
+        jobs.put(ip_str)
+
+    for p in pool:
+        jobs.put(None)
+
+    for p in pool:
+        p.join()
+
+    while not results.empty():
+        ip = results.get()
+        nip = netaddr.IPAddress(ip)
+        ip_list.append(nip)
+
+def load_know_hosts():
+   global know_hosts
+
+   if os.path.isfile(CONFIG_FILE):
+      with open(CONFIG_FILE, "r") as fin:
+          for line in fin:
+              fields = line.strip().split("=")
+              if len(fields) == 2:
+                  mac= fields[0].lstrip().strip()
+                  desc= fields[1].lstrip().strip()
+                  known_hosts[mac] = desc
+
+def print_desc(ip):
+    if mac_map.has_key(ip):
+        mac = mac_map[ip]
+        if known_hosts.has_key(mac):
+            print "%s ====> %s (%s)" %(ip, mac, known_hosts[mac])
+        else:
+            oui = get_oui(mac)
+            company = get_company(oui)
+            print "%s ==> %s (%s)" %(ip, mac, company)
+
+    else:
+        print "%s not found in ARP table!" %(ip)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--net', '-n', default=DEFAULT_NET, help="Subnet")
+
+args = parser.parse_args()
+
+net_parts=args.net.split(".")
+
+subnet=".".join(net_parts[:3])
+
+print "[Scanning network %s.0...]" %(subnet)
+
+do_ping(subnet)
+
+print "[Scanning Complete]"
+
+print "[Reading OUI Map...]"
+init_oui_map()
+
+time.sleep(1)
+load_know_hosts()
+
+print "[Reading ARP Table...]"
+get_mac()
+arp_ip_list=mac_map.keys()
+
+ip_list.sort()
+print "[Hosts responding to ping...]"
+for ip in ip_list:
+    ip_str = str(ip)
+    print_desc(ip_str)
+
+print ""
+print "[Hosts not responding to ping but is in ARP table...]"
+for ip in arp_ip_list:
+    nip = netaddr.IPAddress(ip)
+    if (nip not in ip_list):
+        print_desc(ip)

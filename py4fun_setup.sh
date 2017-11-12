498b15890fa3409343ca87b9d9a6b2f1436b0f1f
Jing Shao <jing_shao@yahoo.com>
2017-11-12 00:14:58 -0600

    Added back file

---
+++ py4fun_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IFS=:
+PY4FUN_PY=imgresize.py:pdfextract.py:pdfmerge.py:picalc.py:prime.py:scpfile.py:sshcmd.py:tk_loan.py:tk_saving.py:tk_tax.py:tk_hash.py:check_cert.py:check_cipher.py:netscan.py:oui.map
+
+pushd $HOME/bin
+
+for i in ${PY4FUN_PY}
+do
+	if [ -e $HOME/github/py4fun/$i ]; then
+		echo ln -sf ../github/py4fun/$i $i
+		ln -sf ../github/py4fun/$i $i
+	fi
+done
+
+popd

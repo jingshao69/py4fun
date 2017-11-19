d415bdf436b99916b9a5dd146368cebb36f4db7e
Jing Shao <jing_shao@yahoo.com>
2017-11-18 19:49:29 -0600

    Added stockq.py

---
+++ stockq.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import argparse
+import requests
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+TIMEOUT=20
+
+ENDP="https://api.iextrading.com/1.0"
+
+def https_get_json(url):
+    r = requests.get(url, verify=False, timeout=TIMEOUT)
+    return r.json()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('stock')
+
+args = parser.parse_args()
+
+url = ENDP + "/stock/" + args.stock + "/quote"
+stock_data = https_get_json(url)
+
+market_cap = stock_data['marketCap']/float(1000000000)
+print "%s %s %.1fB" %(stock_data['latestPrice'], stock_data['change'], market_cap)

2b225c371e1cbc57fbd048b4a82c76909d597d22
Jing Shao <jing_shao@yahoo.com>
2017-11-18 20:00:59 -0600

    Added stockq.py

--- py4fun_setup.sh
+++ py4fun_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=:
-PY4FUN_PY=imgresize.py:pdfextract.py:pdfmerge.py:picalc.py:prime.py:scpfile.py:sshcmd.py:tk_loan.py:tk_saving.py:tk_tax.py:tk_hash.py:check_cert.py:check_cipher.py:netscan.py:oui.map
+PY4FUN_PY=imgresize.py:pdfextract.py:pdfmerge.py:picalc.py:prime.py:scpfile.py:sshcmd.py:tk_loan.py:tk_saving.py:tk_tax.py:tk_hash.py:check_cert.py:check_cipher.py:netscan.py:oui.map:stockq.py
 
 CWD=$(pwd)
 pushd $HOME/bin

946d6a68fe22430cba48ab3761a2e74d2c2f71ba
Jing Shao <jing.shao@spirent.com>
2019-05-21 17:24:11 -0500

    Added scipy setup and linear regression example

---
+++ linregress.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+from scipy import stats
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument('data_file')
+
+args = parser.parse_args()
+
+x=[]
+y=[]
+
+with open(args.data_file, 'r') as f:
+   for line in f:
+       data = line.rstrip().split(' ')
+       if len(data) == 2:
+           x.append(float(data[0]))
+           y.append(float(data[1]))
+
+   if len(x) != 0:
+       slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
+       print " y = %.2f *x + %.2f" %(slope, intercept)
+       print " r = %.2f p = %.2f stderr = %.2f" %(r_value, p_value, std_err)
+
+       
+
+
